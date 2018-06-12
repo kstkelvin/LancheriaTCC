841e1487d1a2b12b5b76825821f0c230c0d4ef23
kstkelvin <kstkelvin@gmail.com>
2018-06-12 20:52:06 -0300

    Fixing bad stuff

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,12 @@
 <footer class="bd-footer">
+  @if(Auth::check())
     <ul class="bd-footer-links">
       <li><a href="https://github.com/kstkelvin/LancheriaTCC">GitHub</a></li>
       <li><a href="https://goo.gl/forms/7X06aUxuT2ALjjJy1">Questionário</a></li>
       <li><a href="/sobre">Sobre a Lancheria</a></li>
     </ul>
-    <p class="text-muted">Website construído por Kelvin da Silva Teixeira</p>
-    <p class="text-muted">Slaughterhouse Software Development &copy;. All rights reserved.</p>
-  </div>
+  @endif
+  <p class="text-muted">Website construído por Kelvin da Silva Teixeira</p>
+  <p class="text-muted">Slaughterhouse Software Development &copy;. All rights reserved.</p>
+</div>
 </footer>
