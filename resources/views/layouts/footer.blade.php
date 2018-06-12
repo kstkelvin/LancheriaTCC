f486f0aaf30b8638dd0ade4c98172db649786126
kstkelvin <kstkelvin@gmail.com>
2018-06-12 20:37:27 -0300

    Questionário Atualizado

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bd-footer">
     <ul class="bd-footer-links">
       <li><a href="https://github.com/kstkelvin/LancheriaTCC">GitHub</a></li>
-      <li><a href="#">Questionário</a></li>
+      <li><a href="https://goo.gl/forms/7X06aUxuT2ALjjJy1">Questionário</a></li>
       <li><a href="/sobre">Sobre a Lancheria</a></li>
     </ul>
     <p class="text-muted">Website construído por Kelvin da Silva Teixeira</p>
